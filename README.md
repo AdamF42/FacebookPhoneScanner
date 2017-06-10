e92ae5d2014cb8badd7560f3867c5a94713e8322
Jacopo Tediosi <jacopotediosi@gmail.com>
2017-06-10 18:32:21 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 id="facebookphonescanner"><b>#FacebookPhoneScanner (Python 2.x)</b></h1>
+
+<ul>
+<li>Questo script esegue un bruteforce sulla ricerca delle persone tramite numero telefonico offerta da <a href="http://www.facebook.com">Facebook.com</a></li>
+<li>Una volta trovato un numero telefonico esistente, il programma si occupa di estrarre nome e cognome ad esso abbinati per poi salvare il tutto in un file .txt</li>
+<li>Sono disponibili due modalità di ricerca: tramite un range di numeri dato in input o tramite una lista di numeri salvata su file (un numero per riga, estensione .txt)</li>
+<li>Entrambe le modalità sono multithread e il numero di thread è selezionabile dall'utente</li>
+</ul>
+
+<p>USO:  <code>pyhton fps.py</code></p>
+
+<p>Prima di avviare lo script è indispensabile modificare i cookie alla riga 17.<br /><br />
+I numeri vanno inseriti con il prefisso nazionale all'inizio ma senza il simbolo "+" (es: 39350XXXXXXX)<br /><br />
+NON tutti i numeri possono essere abbinati ad un nome e cognome, in base alle impostazioni di privacy usate dalle persone (di default la privacy del numero telefonico è impostata su "pubblico")<br /><br />
+E’ inoltre essenziale ricordarsi che Facebook permette di scansionare soltanto qualche centinaio di numeri all’ora, quindi se lo script non trova più risultati dopo vari minuti di esecuzione occorre aspettare il trascorrere dell’ora successiva.</p>
+
+<h4>NON MI ASSUMO ALCUNA RESPONSABILITA' DELL'USO CHE FARETE DI QUESTO SCRIPT</h4>
---
+++ fps.py
@@ -0,0 +1,116 @@
+#Librerie
+import urllib2, re, time, sys, random
+from threading import Thread
+
+#Banner
+print '''
+    ______                __                __      ____  __                        _____                                 
+   / ____/___  ________  / /_  ____  ____  / /__   / __ \/ /_  ____  ____  ___     / ___/_________  ____  ____  ___  _____
+  / /_  / __ \/ ___/ _ \/ __ \/ __ \/ __ \/ //_/  / /_/ / __ \/ __ \/ __ \/ _ \    \__ \/ ___/ __ \/ __ \/ __ \/ _ \/ ___/
+ / __/ / /_/ / /__/  __/ /_/ / /_/ / /_/ / /<    / ____/ / / / /_/ / / / /  __/   ___/ / /__/ /_/ / / / / / / /  __/ /    
+/_/    \____/\___/\___/_____/\____/\____/_/|_|  /_/   /_/ /_/\____/_/ /_/\___/   /____/\___/\____/_/ /_/_/ /_/\___/_/ 
+'''
+
+#Oggetto per fare richieste http
+global opener
+opener = urllib2.build_opener()
+opener.addheaders.append(('Cookie', 'lu=XXX; fr=XXX; c_user=XXX; xs=XXX;'))
+opener.addheaders.append(('User-Agent', 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'))
+
+#Funzione per ottenere il nome corrispondente ad un numero
+def fbCheck(numero):
+	response = opener.open("https://m.facebook.com/search/top/?q=%d" % numero)
+	page_source = str(response.read())	
+	nome = re.compile("<td class=\"bk\"><a[^<]*>([^<]+)").findall(page_source)
+	if len(nome)>0:
+		sys.stdout.write(str(numero)+" = "+str(nome[0])+"\n")
+		with open("lista.txt", "a") as myfile:
+			myfile.write(str(numero)+" = "+str(nome[0])+"\n")
+
+#Controlla la connessione a facebook.com e termina il programma se fallisce
+def controllaConnessione():
+	try:
+		sys.stdout.write("Sto controllando se riesco a collegarmi a Facebook...\n")
+		ret = urllib2.urlopen('https://m.facebook.com/')
+		sys.stdout.write("Connessione riuscita!\n")
+	except Exception, e:
+		print "Non riesco a connettermi a Facebook: "+str(e)+"!\nControlla le impostazioni di rete!\n\nUscendo..."
+		sys.exit()
+
+#Controllo se Facebook e' on
+controllaConnessione()
+#Chiedo la modalita' di scansione (range, file o numero parziale)
+mode = input("\n\nScegli la modalita':\n1. Range\n2. Da file\n3. Conoscendo una parte del numero\nScelta: ")
+if mode == 1:
+	threads = input("\nThread: ")
+	startn	= input("Numero di partenza: ")
+	endn	= input("Numero di fine: ")
+	finiti	= 0
+	print "\n"
+	if endn-startn+1>0:
+		class newThread(Thread):
+			def __init__(self):
+				Thread.__init__(self)
+				self.daemon = True
+				self.start()
+			def run(self):
+				global threads, startn, endn, finiti
+				while True:
+					#Richiesta http
+					if startn<=endn:
+						fbCheck(startn)
+						startn=startn+1
+					else:
+						finiti=finiti+1
+						break
+		for nt in range(threads):
+			newThread()
+			startn=startn+1
+		try:
+			while True:
+				if finiti>=threads:
+					print "\nFinito!...\n"
+					exit()
+				time.sleep(1)
+		except KeyboardInterrupt:
+			print "\nUscendo...\n"
+	else:
+		print "\nIl numero iniziale deve essere inferiore o uguale a quello finale! Uscendo...\n"
+elif mode == 2:
+	filename = raw_input("\nInput file: ")
+	file=open(filename)
+	file=file.readlines()
+	numerolinea=0
+	numerolineamax=len(file)
+	threads = input("Thread: ")
+	finiti=0
+	print "\n"
+	class newThread(Thread):
+		def __init__(self):
+			Thread.__init__(self)
+			self.daemon = True
+			self.start()
+		def run(self):
+			global numerolinea,numerolineamax,finiti
+			while True:
+				#Richiesta http
+				if numerolinea<numerolineamax:
+					numero=file[numerolinea].rstrip()
+					fbCheck(int(numero))
+					numerolinea=numerolinea+1
+				else:
+					finiti=finiti+1
+					break
+	for nt in range(threads):
+		newThread()
+		numerolinea=numerolinea+1
+	try:
+		while True:
+			if finiti>=threads:
+				print "\nFinito!...\n"
+				exit()
+			time.sleep(1)
+	except KeyboardInterrupt:
+		print "\nUscendo...\n"
+else:
+	print "Inserisci una scelta valida! Uscendo...\n"
